7ff42f639c25ec82e2650b4b4d310a941eb9bdd4
jayanth800 <hrjayanth5@gmail.com>
2026-03-07 19:16:18 +0530

    Add files via upload

---
+++ jayanth/task1d1.py
@@ -0,0 +1,28 @@
+#integer
+age = 20
+print(age)
+print(type(age))
+
+
+#float
+price = 99.5
+print(price)
+print(type(price))
+
+
+#string
+name = "Jayanth"
+print(name)
+print(type(name))
+
+
+#boolean
+is_student = True
+print(is_student)
+print(type(is_student))
+
+
+#list
+numbers = [10, 20, 30, 40]
+print(numbers)
+print(type(numbers))
